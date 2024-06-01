5f511af1ab758286a8c786501631443b9eb1745d
RokuHanmar <90313219+RokuHanmar@users.noreply.github.com>
2024-06-01 22:51:40 +0100

    Create Healing Word.py

---
+++ Healing Word.py
@@ -0,0 +1,27 @@
+# Import libraries
+import random
+
+# Define dice functions
+def d4():
+    d4 = random.randint(1,4)
+    return d4
+
+# Define external modifiers - CHANGE AS NECESSARY
+proficiencyBonus = 4
+wisdomBonus = 0
+spellcastingModifier = proficiencyBonus + wisdomBonus
+
+
+# Healing Word
+def healingWord():
+    total = 0
+    level = int(input("What level are you casting the spell at? "))
+    for i in range (level):
+        roll = d4()
+        print(roll, end=" ")
+        total += roll
+    total += spellcastingModifier
+    print("\n")
+    print(total)
+        
+healingWord()
