f7bc2682b748e80a2d51887202423386fc1201fa
RokuHanmar <90313219+RokuHanmar@users.noreply.github.com>
2024-06-01 22:57:11 +0100

    Update Healing Word.py
    Renamed wisdomBonus to abilityBonus to make it class agnostic
    
    Removed healingWord function and added the code to main program body

--- Healing Word.py
+++ Healing Word.py
@@ -8,22 +8,18 @@ def d4():
 
 # Define external modifiers - CHANGE AS NECESSARY
 proficiencyBonus = 4
-wisdomBonus = 0
-spellcastingModifier = proficiencyBonus + wisdomBonus
-
+abilityBonus = 0
+spellcastingModifier = proficiencyBonus + abilityBonus
 
 # Healing Word
-def healingWord():
-    total = 0
-    level = 0
-    while level > 9 or level < 1:
-        level = int(input("What level are you casting the spell at? Level must be between 1 and 9 inclusive "))    
-    for i in range (level):
-        roll = d4()
-        print(roll, end=" ")
-        total += roll
-    total += spellcastingModifier
-    print("\n")
-    print(total)
-        
-healingWord()
+total = 0
+level = 0
+while level > 9 or level < 1:
+    level = int(input("What level are you casting the spell at? Level must be between 1 and 9 inclusive "))    
+for i in range (level):
+    roll = d4()
+    print(roll, end=" ")
+    total += roll
+total += spellcastingModifier
+print("\n")
+print(total)
