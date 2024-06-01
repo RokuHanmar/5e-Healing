5176ac8adcb2c3994d080d1f4dd13cbbf3164645
RokuHanmar <90313219+RokuHanmar@users.noreply.github.com>
2024-06-02 00:05:53 +0100

    Create Regenerate.py

---
+++ Regenerate.py
@@ -0,0 +1,39 @@
+# Import libraries
+import random
+
+# Define dice functions
+def d8():
+    d8 = random.randint(1,8)
+    return d8
+
+# Define external modifiers - CHANGE AS NECESSARY
+proficiencyBonus = 4
+abilityBonus = 0
+spellcastingModifier = proficiencyBonus + abilityBonus
+domain = "Life"
+
+# Define spell variables
+total = 0
+level = 0
+
+# Verify the spell is being cast at a valid level
+while level > 9 or level < 7:
+    level = int(input("What level are you casting the spell at? Level must be between 7 and 9 inclusive "))    
+
+# Roll and output dice, then add to a running total
+for i in range (4):
+    roll = d8()
+    print(roll, end=" ")
+    total += roll
+
+# Calculate and output final total
+if domain == "Life":
+    lifeBonus = 2 + level
+else:
+    lifeBonus = 0
+
+print("\n")
+print("Total: " + str(total))
+print("Domain bonus: " + str(lifeBonus))
+total += lifeBonus + 15
+print("Grand total: " + str(total))
